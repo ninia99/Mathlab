617d388346f2b08e167e25c41bd1f21942508f84
ninia99 <ninoshvelidze78@gmail.com>
2024-03-30 23:40:27 +0100

    change entire templates

--- mathlab/pages/views.py
+++ mathlab/pages/views.py
@@ -2,25 +2,4 @@ from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from django.views.generic import TemplateView
 from django.db import models
-
 from .models import Post
-
-
-def blog_view(request):
-    posts = Post.objects.all()
-    context = {
-        "posts": posts,
-    }
-    return render(request, template_name="home/home.html", context=context)
-
-
-def blog_category(request, category):
-    context = {
-        "category": category,
-        "posts": Post
-
-    }
-    return render(request, template_name="home/home.html", context=context)
-
-def  demo(request):
-    pass
