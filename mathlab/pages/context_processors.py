f48c08a9b0a303b5cd3584659f0318cac421836f
sonishve <sonishve@git.uni-due.de>
2024-09-06 17:11:43 +0200

    minor change in context_processor

--- mathlab/pages/context_processors.py
+++ mathlab/pages/context_processors.py
@@ -3,4 +3,4 @@ from .models import SiteSettings
 
 def site_settings(request):
     settings = SiteSettings.objects.filter()
-    return {'site_settings': settings.first()}
+    return {'site_settings': settings.all()}
